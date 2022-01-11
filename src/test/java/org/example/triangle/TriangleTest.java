9c0542f88bd95fc1b90a02dc115a03d8a36b5f2c
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-01-11 17:27:30 +0100

    Add tests for Triangle

---
+++ src/test/java/org/example/triangle/TriangleTest.java
@@ -0,0 +1,65 @@
+package org.example.triangle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TriangleTest {
+
+    @Test
+    void aTriangleWithThreeEqualSidesIsAnEquilateral() {
+        Triangle triangle = new Triangle(2.0, 2.0, 2.0);
+
+        boolean isEquilateral = triangle.isEquilateral();
+
+        assertTrue(isEquilateral, "A triangle with equal sides is an equilateral triangle.");
+    }
+
+    @Test
+    void aTriangleWithThreeEqualSidesIsNotAnIsosceles() {
+        Triangle triangle = new Triangle(2.0, 2.0, 2.0);
+
+        boolean isIsosceles = triangle.isIsosceles();
+
+        assertFalse(isIsosceles, "A triangle with equal sides is NOT an isosceles triangle.");
+    }
+
+    @Test
+    void aTriangleWithThreeEqualSidesIsNotAScalene() {
+        Triangle triangle = new Triangle(2.0, 2.0, 2.0);
+
+        boolean isScalene = triangle.isScalene();
+
+        assertFalse(isScalene, "A triangle with equal sides is not a scalene triangle.");
+    }
+
+    @Test
+    void anArrayWithOnlyTwoEqualSidesShouldReturnAnIsoscelesTriangle() {
+        Triangle triangle = new Triangle(new double[]{2.5, 2.5, 3.0});
+
+        boolean isIsosceles = triangle.isIsosceles();
+
+        assertTrue(isIsosceles, "A triangle with 2 equal sides is an isosceles triangle.");
+    }
+
+    @Test
+    void aTriangleFromThreePointsWithUnequalLengthsIsAScalene() {
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(2, 7), new Point(3, 9));
+
+        boolean isScalene = triangle.isScalene();
+
+        assertTrue(isScalene, "A triangle with 3 unequal is a scalene triangle.");
+    }
+
+    @Test
+    void anArrayOfThreePointsWithUnequalLengthsIsAScalene() {
+        Point[] points = {new Point(0, 0), new Point(2, 7), new Point(3, 9)};
+        Triangle triangle = new Triangle(points);
+
+        boolean isEquilateral = triangle.isEquilateral();
+
+        assertFalse(isEquilateral, "A triangle with 3 unequal is NOT an equilateral triangle.");
+    }
+
+}
