abdd7b2b4b8c891da3430dbd1a1edc55c4c44a9e
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-01-13 10:49:57 +0100

    Nest tests & add DisplayName

--- src/test/java/org/example/triangle/TriangleTest.java
+++ src/test/java/org/example/triangle/TriangleTest.java
@@ -1,5 +1,7 @@
 package org.example.triangle;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -7,60 +9,73 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TriangleTest {
 
-    @Test
-    void aTriangleWithThreeEqualSidesIsAnEquilateral() {
-        Triangle triangle = new Triangle(2.0, 2.0, 2.0);
+    @Nested
+    class Isosceles {
+        @Test
+        void anArrayWithOnlyTwoEqualSidesShouldReturnAnIsoscelesTriangle() {
+            Triangle triangle = new Triangle(new double[]{2.5, 2.5, 3.0});
 
-        boolean isEquilateral = triangle.isEquilateral();
+            boolean isIsosceles = triangle.isIsosceles();
 
-        assertTrue(isEquilateral, "A triangle with equal sides is an equilateral triangle.");
+            assertTrue(isIsosceles, "A triangle with 2 equal sides is an isosceles triangle.");
+        }
     }
 
-    @Test
-    void aTriangleWithThreeEqualSidesIsNotAnIsosceles() {
-        Triangle triangle = new Triangle(2.0, 2.0, 2.0);
+    @Nested
+    class Equilateral {
+        @Test
+        @DisplayName("is an equilateral")
+        void aTriangleWithThreeEqualSidesIsAnEquilateral() {
+            Triangle triangle = new Triangle(2.0, 2.0, 2.0);
 
-        boolean isIsosceles = triangle.isIsosceles();
+            boolean isEquilateral = triangle.isEquilateral();
 
-        assertFalse(isIsosceles, "A triangle with equal sides is NOT an isosceles triangle.");
+            assertTrue(isEquilateral, "A triangle with equal sides is an equilateral triangle.");
+        }
+
+        @Test
+        @DisplayName("is not an isosceles")
+        void aTriangleWithThreeEqualSidesIsNotAnIsosceles() {
+            Triangle triangle = new Triangle(2.0, 2.0, 2.0);
+
+            boolean isIsosceles = triangle.isIsosceles();
+
+            assertFalse(isIsosceles, "A triangle with equal sides is NOT an isosceles triangle.");
+        }
+
+        @Test
+        @DisplayName("is not a scalene")
+        void aTriangleWithThreeEqualSidesIsNotAScalene() {
+            Triangle triangle = new Triangle(2.0, 2.0, 2.0);
+
+            boolean isScalene = triangle.isScalene();
+
+            assertFalse(isScalene, "A triangle with equal sides is not a scalene triangle.");
+        }
     }
 
-    @Test
-    void aTriangleWithThreeEqualSidesIsNotAScalene() {
-        Triangle triangle = new Triangle(2.0, 2.0, 2.0);
+    @Nested
+    class Scalene {
+        @Test
+        void aTriangleFromThreePointsWithUnequalLengthsIsAScalene() {
+            Triangle triangle = new Triangle(new Point(0, 0), new Point(2, 7), new Point(3, 9));
 
-        boolean isScalene = triangle.isScalene();
+            boolean isScalene = triangle.isScalene();
 
-        assertFalse(isScalene, "A triangle with equal sides is not a scalene triangle.");
+            assertTrue(isScalene, "A triangle with 3 unequal sides is a scalene triangle.");
+        }
+
+        @Test
+        void anArrayOfThreePointsWithUnequalLengthsIsAScalene() {
+            Point[] points = {new Point(0, 0), new Point(2, 7), new Point(3, 9)};
+            Triangle triangle = new Triangle(points);
+
+            boolean isEquilateral = triangle.isEquilateral();
+
+            assertFalse(isEquilateral, "A triangle with 3 unequal sides is NOT an equilateral triangle.");
+        }
     }
 
-    @Test
-    void anArrayWithOnlyTwoEqualSidesShouldReturnAnIsoscelesTriangle() {
-        Triangle triangle = new Triangle(new double[]{2.5, 2.5, 3.0});
-
-        boolean isIsosceles = triangle.isIsosceles();
-
-        assertTrue(isIsosceles, "A triangle with 2 equal sides is an isosceles triangle.");
-    }
-
-    @Test
-    void aTriangleFromThreePointsWithUnequalLengthsIsAScalene() {
-        Triangle triangle = new Triangle(new Point(0, 0), new Point(2, 7), new Point(3, 9));
-
-        boolean isScalene = triangle.isScalene();
-
-        assertTrue(isScalene, "A triangle with 3 unequal sides is a scalene triangle.");
-    }
-
-    @Test
-    void anArrayOfThreePointsWithUnequalLengthsIsAScalene() {
-        Point[] points = {new Point(0, 0), new Point(2, 7), new Point(3, 9)};
-        Triangle triangle = new Triangle(points);
-
-        boolean isEquilateral = triangle.isEquilateral();
-
-        assertFalse(isEquilateral, "A triangle with 3 unequal sides is NOT an equilateral triangle.");
-    }
 
     //Naming suggestions
     //withThreeEqualSidesShouldNotBeAnIsosceles BEST (because dropped "triangle" in method name)
